e8c06963410f0c5024770ae9e44d39f9505b2f4e
Jakob Steiner <kosmoz@users.noreply.github.com>
2026-02-03 16:15:42 +0100

    chore: add escaping newline characters in env secrets (#1760)
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>
    Co-authored-by: Philip Miglinci <pmig@glasskube.com>

--- internal/deploymentvalues/env.go
+++ internal/deploymentvalues/env.go
@@ -2,6 +2,7 @@ package deploymentvalues
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/distr-sh/distr/internal/types"
 )
@@ -15,9 +16,16 @@ func EnvFileReplaceSecrets(d EnvFileDataAccessor, secrets []types.SecretWithUpda
 		return nil, nil
 	} else if tpl, err := parseTemplateBytes("envFile", data); err != nil {
 		return nil, fmt.Errorf("deployment env file template parsing error: %w", err)
-	} else if data, err := executeTemplate(tpl, getTemplateData(secrets)); err != nil {
+	} else if data, err := executeTemplate(
+		tpl,
+		getTemplateData(secrets, withValueInterceptor(escapeNewlines)),
+	); err != nil {
 		return nil, fmt.Errorf("deployment env file template execution error: %w", err)
 	} else {
 		return data, nil
 	}
+}
+
+func escapeNewlines(v string) string {
+	return strings.ReplaceAll(v, "\n", "\\n")
 }
--- internal/deploymentvalues/template.go
+++ internal/deploymentvalues/template.go
@@ -11,13 +11,44 @@ type templateData struct {
 	Secrets map[string]string
 }
 
-func getTemplateData(secrets []types.SecretWithUpdatedBy) templateData {
+type templateDataConfig struct {
+	valueInterceptor func(string) string
+}
+
+type templateDataOption = func(*templateDataConfig)
+
+func chain[T any](fns ...func(T) T) func(T) T {
+	return func(v T) T {
+		for _, f := range fns {
+			if f != nil {
+				v = f(v)
+			}
+		}
+		return v
+	}
+}
+
+func ident[T any](v T) T { return v }
+
+func withValueInterceptor(f func(string) string) templateDataOption {
+	return func(config *templateDataConfig) {
+		config.valueInterceptor = chain(config.valueInterceptor, f)
+	}
+}
+
+func getTemplateData(secrets []types.SecretWithUpdatedBy, options ...templateDataOption) templateData {
+	config := templateDataConfig{valueInterceptor: ident[string]}
+	for _, opt := range options {
+		opt(&config)
+	}
+
 	data := templateData{
 		Secrets: make(map[string]string),
 	}
 	for _, secret := range secrets {
-		data.Secrets[secret.Key] = secret.Value
+		data.Secrets[secret.Key] = config.valueInterceptor(secret.Value)
 	}
+
 	return data
 }
 
